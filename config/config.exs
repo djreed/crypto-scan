7c9980e7c94c7dd0ac372f8f7c8ae00ca933f6c2
djreed <ddavidreed@gmail.com>
2017-11-18 17:46:57 -0500

    Import API key

--- config/config.exs
+++ config/config.exs
@@ -22,6 +22,8 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# import email api data
+import_config "email_api.secret.exs"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
